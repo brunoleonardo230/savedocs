c955bee150e21a90a1960dfbf3eb92ba370321e5
Salathiel Serra <thiel.serra@gmail.com>
2022-05-30 20:02:43 -0300

    Atualizando arquivo de layout para usuários autenticados

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,31 +9,49 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        <link rel="stylesheet" href="{{ asset('themes/vendor/fontawesome-free/css/all.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('themes/css/sb-admin-2.css') }}">
+        
 
         {{-- Lib Stripe --}}
         <script src="https://js.stripe.com/v3/"></script>
+
+        {{-- @livewireStyles --}}
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+    <body id="page-top">
+        <div id="wrapper">
             @include('layouts.navigation')
 
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
+            <!-- Content Wrapper -->
+            <div id="content-wrapper" class="d-flex flex-column">
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+                <!-- Main Content -->
+                <div id="content">
+                    @include('layouts.navbar')
+
+                    <div class="container-fluid">
+
+                        <!-- Page Heading -->
+                        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                            <h1 class="h3 mb-0 text-gray-800"> {{ $header }} </h1>
+                        </div>
+
+                        {{ $slot }}
+                    </div>
+                </div>
+            </div>
         </div>
+
+        <!-- Bootstrap core JavaScript-->
+        <script src="{{ asset('themes/vendor/jquery/jquery.min.js') }}"></script>
+        <script src="{{ asset('themes/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+        <!-- Core plugin JavaScript-->
+        <script src="{{ asset('themes/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+        <!-- Custom scripts for all pages-->
+        <script src="{{ asset('themes/js/sb-admin-2.min.js') }}"></script>
+
+        {{-- @livewireScripts --}}
+
     </body>
 </html>
