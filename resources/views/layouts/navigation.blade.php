2f55bbac77f3ce91011e4223ebad071c30511273
Salathiel Serra <thiel.serra@gmail.com>
2022-05-30 21:17:25 -0300

    Ajustes no layout

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -34,6 +34,12 @@
             <i class="fas fa-fw fa-cog"></i>
             <span>Usuários</span>
         </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                {{-- <h6 class="collapse-header"></h6> --}}
+                <a class="collapse-item" href="buttons.html">Criar usuário</a>
+            </div>
+        </div>
     </li>
 
 
