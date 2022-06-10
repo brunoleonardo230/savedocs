44f36ae5a2d373e44ef23f5ac78e7fd382b93cbc
Salathiel Serra <thiel.serra@gmail.com>
2022-06-10 01:27:48 -0300

    Adicionando arquivos para controle de usuários, perfis e permissões pelo usuário administrador

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use App\Http\Controllers\SiteController;
 use App\Http\Controllers\PortalController;
 use App\Http\Controllers\Subscription\SubscriptionController;
-use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\{
+    UserController, RoleController, ResourceController };
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PortalController::class, 'index'])->name('portal.index');
@@ -35,6 +36,28 @@ Route::get('laravel-version', function() {
     return "Your Laravel version is ".$laravel::VERSION;
 });
 
+
+
 Route::group([ 'middleware' => 'access.control.list'], function() {
-    Route::get('/user/index', [UserController::class, 'index'])->name('users.index');
+
+    Route::get('/users/index', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/edit/{id}', [UserController::class, 'edit'])->name('users.edit');
+    Route::put('/users/update/{id}', [UserController::class, 'update'])->name('users.update');
+
+    Route::get('/roles/index', [RoleController::class, 'index'])->name('roles.index');
+    Route::get('/roles/create', [RoleController::class, 'create'])->name('roles.create');
+    Route::post('/roles/store', [RoleController::class, 'store'])->name('roles.store');
+    Route::get('/roles/show/{id}', [RoleController::class, 'show'])->name('roles.show');
+    Route::put('/roles/update/{id}', [RoleController::class, 'update'])->name('roles.update');
+    Route::delete('/roles/destroy/{id}', [RoleController::class, 'destroy'])->name('roles.destroy');
+	Route::get('/roles/{role}/resources', [RoleController::class, 'syncResources'])->name('roles.resources');
+	Route::put('/roles/{role}/resources', [RoleController::class, 'updateSyncResources'])->name('roles.resources.update');
+
+    Route::get('/resources/index', [RoleController::class, 'index'])->name('resources.index');
+    Route::get('/resources/create', [RoleController::class, 'create'])->name('resources.create');
+    Route::post('/resources/store', [RoleController::class, 'store'])->name('resources.store');
+    Route::get('/resources/show/{id}', [RoleController::class, 'show'])->name('resources.show');
+    Route::put('/resources/update/{id}', [RoleController::class, 'update'])->name('resources.update');
+    Route::delete('/resources/destroy/{id}', [RoleController::class, 'destroy'])->name('resources.destroy');   
+    
 });
