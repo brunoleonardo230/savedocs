4594090b941eba832f82c99a6a63aef8f4d069d3
Salathiel Serra <thiel.serra@gmail.com>
2022-06-12 10:34:59 -0300

    Refatorando rotas

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-use App\Http\Controllers\SiteController;
-use App\Http\Controllers\PortalController;
-use App\Http\Controllers\Subscription\SubscriptionController;
+use App\Http\Controllers\ {
+    SiteController, PortalController, CallController };
 use App\Http\Controllers\Admin\{
-    UserController, RoleController, ResourceController };
+        UserController, RoleController, ResourceController };
+use App\Http\Controllers\Subscription\SubscriptionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PortalController::class, 'index'])->name('portal.index');
@@ -41,32 +41,14 @@ Route::get('laravel-version', function() {
 
 
 
-Route::group([ 'middleware' => 'access.control.list'], function() {
+Route::group([ 'middleware' => ['auth','access.control.list']], function() {
+    Route::resource('users', UserController::class);
 
-    Route::get('/users/index', [UserController::class, 'index'])->name('users.index');
-    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-    Route::post('/users/store', [UserController::class, 'store'])->name('users.store');
-    Route::get('/users/show/{id}', [UserController::class, 'show'])->name('users.show');
-    Route::get('/users/edit/{id}', [UserController::class, 'edit'])->name('users.edit');
-    Route::put('/users/update/{id}', [UserController::class, 'update'])->name('users.update');
-    Route::delete('/users/destroy/{id}', [UserController::class, 'destroy'])->name('users.destroy');
-
-    Route::get('/roles/index', [RoleController::class, 'index'])->name('roles.index');
-    Route::get('/roles/create', [RoleController::class, 'create'])->name('roles.create');
-    Route::post('/roles/store', [RoleController::class, 'store'])->name('roles.store');
-    Route::get('/roles/show/{id}', [RoleController::class, 'show'])->name('roles.show');
-    Route::get('/roles/edit/{id}', [RoleController::class, 'edit'])->name('roles.edit');
-    Route::put('/roles/update/{id}', [RoleController::class, 'update'])->name('roles.update');
-    Route::delete('/roles/destroy/{id}', [RoleController::class, 'destroy'])->name('roles.destroy');
-	Route::get('/roles/{role}/resources', [RoleController::class, 'syncResources'])->name('roles.resources');
+    Route::resource('roles', RoleController::class);
+    Route::get('/roles/{role}/resources', [RoleController::class, 'syncResources'])->name('roles.resources');
 	Route::put('/roles/{role}/resources', [RoleController::class, 'updateSyncResources'])->name('roles.resources.update');
 
-    Route::get('/resources/index', [ResourceController::class, 'index'])->name('resources.index');
-    Route::get('/resources/create', [ResourceController::class, 'create'])->name('resources.create');
-    Route::post('/resources/store', [ResourceController::class, 'store'])->name('resources.store');
-    Route::get('/resources/show/{id}', [ResourceController::class, 'show'])->name('resources.show');
-    Route::get('/resources/edit/{id}', [ResourceController::class, 'edit'])->name('resources.edit');
-    Route::put('/resources/update/{id}', [ResourceController::class, 'update'])->name('resources.update');
-    Route::delete('/resources/destroy/{id}', [ResourceController::class, 'destroy'])->name('resources.destroy');   
-    
+    Route::resource('resources', ResourceController::class);
+
+    Route::resource('callers', CallController::class);
 });
