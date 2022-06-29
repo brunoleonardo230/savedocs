262f6b14228ce5b40d1163f76a1ef9372f052dad
Bruno Leonardo <bruno.leonardo23@hotmail.com>
2022-06-29 13:14:16 -0300

    modal de comentários do ticket

--- resources/views/admin/tickets/edit.blade.php
+++ resources/views/admin/tickets/edit.blade.php
@@ -6,7 +6,7 @@
         </h2>        
     </x-slot>
     <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-8">
             <div class="card">                            
                 <div class="card-body">
                     <h5 class="card-title">
@@ -20,57 +20,79 @@
                     <p> <strong> Email:</strong> <small> {{$ticket->author_email}} </small></p>
                     <p> <strong> Prioridade:</strong> <small> {{$ticket->priority->name}} </small></p>                   
 
-                </div>                
-                <button type="button" class="btn btn-outline-success" onclick="Mudarestado('minhaDiv')">Alterar Status</button>
+                </div>      
+                <!-- Button trigger modal -->
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                    Alterar Status
+                </button>          
+                <!-- <button type="button" class="btn btn-outline-success" onclick="Mudarestado('minhaDiv')">Alterar Status</button> -->
             </div>
         </div>
     </div>
-    <div class="row" id="minhaDiv" style="display: none;">
-        <div class="col-md-6">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">
-                        <p>
-                            <strong> Alterar Status </strong>                    
-                        </p>
-                    </h5>
-                    <form action="{{route('services.store')}}"  method="post">
-                        @csrf
-                            <div class="row form-group">            
-                                <div class="col-md-12">
-                                    <label>Status: <span class="text-danger">*</span> </label>
-                                    <select name="status_id" class="form-control" required>
-                                        <option value=""> -- Selecione -- </option>
-                                        @foreach($statuses as $status)
-                                            <option value="{{$status->id}}">{{$status->name}}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
+    <br>
+    <div class="row">
+        <div class="col-md-12">
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-8 col-md-8 mb-4">
+                <div class="card border-left-primary shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                    LUCROS (MENSAIS)</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
                             </div>
-                            <div class="row form-group">
-                                <div class="col-md-12">
-                                    <label>Comentário: <span class="text-danger">*</span> </label>
-                                    <textarea id="comment_text" name="comment_text" class="form-control ">{{ old('comment_text', isset($commet) ? $commet->comment_text : '') }}</textarea>
-                                </div>
+                            <div class="col-auto">
+                                <i class="fas fa-calendar fa-2x text-gray-300"></i>
                             </div>
-                            <p class="card-text">
-                                <a href="{{route('tickets.edit', $ticket->id)}}" class="btn btn-outline-success">
-                                    Confirmar
-                                </a>                               
-                            </p>
-                    </form>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Alterar Status</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="{{route('services.store')}}"  method="post">
+                    @csrf
+                        <div class="row form-group">            
+                            <div class="col-md-12">
+                                <label>Status: <span class="text-danger">*</span> </label>
+                                <select name="status_id" class="form-control" required>
+                                    <option value=""> -- Selecione -- </option>
+                                    @foreach($statuses as $status)
+                                        <option value="{{$status->id}}">{{$status->name}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        <div class="row form-group">
+                            <div class="col-md-12">
+                                <label>Comentário: <span class="text-danger">*</span> </label>
+                                <textarea id="comment_text" name="comment_text" class="form-control ">{{ old('comment_text', isset($commet) ? $commet->comment_text : '') }}</textarea>
+                            </div>
+                        </div>                    
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                    <a href="{{route('tickets.edit', $ticket->id)}}" class="btn btn-outline-success">
+                        Confirmar
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    
 </x-app-layout>
 
-<script>
-    function Mudarestado(el) {
-        var display = document.getElementById(el).style.display;
-        if(display == "none")
-            document.getElementById(el).style.display = 'block';
-        else
-            document.getElementById(el).style.display = 'none';
-    }
-</script>
